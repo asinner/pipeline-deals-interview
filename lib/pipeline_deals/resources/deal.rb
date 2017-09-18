f3eff4d3e7b8d8c8dedf9c3daf47176ab3e83375
Andrew Sinner <andrewsinner@gmail.com>
2017-09-17 19:58:33 -0700

    Fix Deal not extending correct class

--- lib/pipeline_deals/resources/deal.rb
+++ lib/pipeline_deals/resources/deal.rb
@@ -1,8 +1,8 @@
 module PipelineDeals
   # The deal class acts as the client library that will interact with
   # the PipelineDeals deals API. Since the PipelineDeals API is RESTful, this
-  # class will be supported by the ActiveResource::Base class.
-  class Deal < ActiveResource::Resource
+  # class will be supported by the Resource class.
+  class Deal < Resource
     self.collection_parser = DealCollection    
   end
 end
